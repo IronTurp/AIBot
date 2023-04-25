01b898e83f05223b777b9eda3df12991622fe4a3
IronTurp <123204948+IronTurp@users.noreply.github.com>
2023-04-25 13:47:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,53 @@ import torch.nn as nn
 from torchvision.models import resnet18
 
 class CustomModel(nn.Module):
-    def __init__(self, num_key_classes, num_mouse_classes):
+    def __init__(self, num_key_classes):
         super(CustomModel, self).__init__()
         
-        # Load the pretrained ResNet18 model and remove the last layer to use it as a feature extractor
+        # Define the feature extractor (ResNet18) and remove the last layer
         self.feature_extractor = resnet18(pretrained=True)
         self.feature_extractor = nn.Sequential(*list(self.feature_extractor.children())[:-1])
         
-        # Define the classifier for keyboard key presses
-        self.keyboard_classifier = nn.Sequential(
-            nn.Linear(512, 256),  # Linear layer with 512 input features and 256 output features
-            nn.ReLU(),           # ReLU activation function
-            nn.Linear(256, num_key_classes)  # Linear layer with 256 input features and output features equal to the number of key classes
+        # Define the combined representation layer
+        # It takes the concatenated image features (512), mouse data (4), and one-hot encoded keyboard data
+        # and reduces the dimensionality to 256
+        self.combined_representation = nn.Sequential(
+            nn.Linear(512 + 4 + num_key_classes, 256),
+            nn.ReLU()
         )
         
-        # Define the classifier for mouse movements
-        self.mouse_classifier = nn.Sequential(
-            nn.Linear(512 + 4, 256),  # Linear layer with 516 input features (512 from feature_extractor + 4 from mouse_data) and 256 output features
-            nn.ReLU(),                # ReLU activation function
-            nn.Linear(256, num_mouse_classes)  # Linear layer with 256 input features and output features equal to the number of mouse classes
+        # Define the keyboard classifier
+        # It takes the 256-dimensional joint representation and predicts the keyboard key press probabilities
+        self.keyboard_classifier = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, num_key_classes)
+        )
+        
+        # Define the mouse regression network
+        # It takes the 256-dimensional joint representation and predicts the continuous values
+        # for the change in x and y coordinates of the mouse movement
+        self.mouse_regression = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 2)  # The output size is now 2 (x and y coordinate changes)
         )
     
-    def forward(self, image, mouse_data):
-        # Pass the input image through the feature extractor
+    def forward(self, image, mouse_data, keyboard_data):
+        # Pass the input image through the feature extractor to obtain a 512-dimensional feature vector
         features = self.feature_extractor(image).view(-1, 512)
         
-        # Use the extracted features as input for the keyboard key press classifier
-        keyboard_output = self.keyboard_classifier(features)
+        # Concatenate the 512-dimensional feature vector, mouse data, and one-hot encoded keyboard data
+        # Pass the concatenated vector through the combined_representation layer
+        combined = self.combined_representation(torch.cat((features, mouse_data, keyboard_data), dim=1))
         
-        # Concatenate the extracted features with the mouse_data and use it as input for the mouse movement classifier
-        mouse_output = self.mouse_classifier(torch.cat((features, mouse_data), dim=1))
+        # Pass the 256-dimensional joint representation through the keyboard classifier
+        # to obtain the keyboard key press probabilities
+        keyboard_output = self.keyboard_classifier(combined)
         
+        # Pass the 256-dimensional joint representation through the mouse regression network
+        # to obtain the predicted change in x and y coordinates of the mouse movement
+        mouse_output = self.mouse_regression(combined)
+        
+        # Return the keyboard key press probabilities and the mouse movement (delta_x, delta_y)
         return keyboard_output, mouse_output
